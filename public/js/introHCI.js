d2f589743c210f7a1493b01f742d4e91101abc90
John Daniel Domine <jdomine@ucsd.edu>
2018-02-15 11:05:22 -0800

    update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,9 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+	
+
+	//$("a").click()
 }
 
 /*
@@ -27,4 +30,25 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	//a.preventDefault();
+
+	var projectID = $(this).closest('.project').attr('id');
+
+	$.get("/project/" + projectID, callbackFxn)
+	console.log("something")
 }
+
+function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>';
+}
+
+function callbackFxn(result){
+	console.log(result);
+}
+
+
